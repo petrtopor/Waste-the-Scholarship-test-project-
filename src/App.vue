c56a5d9e01542a4e453301b7d3b2ef436e9acf02
coder.ru@gmail.com <coder.ru@gmail.com>
2018-06-09 06:42:39 +0300

    свёрстана главная страница (без разбиения на компоненты)

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
 
             <div class="col-1 price">
               <p>Размер стипендии:</p>
-              <input type="text" placeholder="2000"/>
+              <input type="text" placeholder="2000" :value="secondData" />
               <p class="ruble-sign">&#8399;</p>
             </div>
 
@@ -35,66 +35,185 @@
             <div class="main-space">
                 <div class="row spacer">
 
-                    <div class="col-1">
-                        <div id="back-to-list">
-                               <p><< Вернуться к списку</p>
-                        </div>
-                        <div id="caption">
-                            <h2>Добавление места</h2>
+                    <div class="col-2">
+                        <div class="scholarshipPercentage">
+                            <p>Сколько: 20% - 70% стипендии</p>
+                            <div class="scholarshipSlider">
+                                <!--
+                                <div class="line"></div>
+                                <div class="start"></div>
+                                <div class="end"></div>
+                                -->
+                            </div>
                         </div>
                     </div>
 
-                    <div class="col-2">
-
+                    <div class="col-1">
+                        <div id="how-and-where">
+                            <div id="how">
+                                <p>Как:</p>
+                                <div id="stars">
+                                    <div class="star marked"></div>
+                                    <div class="star marked"></div>
+                                    <div class="star marked"></div>
+                                    <div class="star not-marked"></div>
+                                    <div class="star not-marked"></div>
+                                </div>
+                            </div>
+                            <div id="where">
+                                <p>Где:</p>
+                                <select>
+                                    <option>Ресторан</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
                 <div class="row">
-                    <div class="col-1">
-                        <section id="form">
 
-                            <div id="name" class="input-field">
-                                <p>Название:</p>
-                                <input type="text" placeholder="Введите название места"/>
-                            </div>
-
-                            <div id="address" class="input-field">
-                                <p>Адрес:</p>
-                                <input type="text" placeholder="Введите точный адрес места"/>
-                            </div>
-
-                            <div id="average-bill" class="input-field">
-                                <p>Средний чек:</p>
-                                <input type="number" placeholder="Введите размер среднего чека, руб."/>
-                            </div>
-
-                            <div id="category" class="input-field">
-                                <p>Категория:</p>
-                                <select name="category" id="category-select" >
-                                    <option value="1">Бар</option>
-                                    <option value="2">Кафе</option>
-                                    <option value="3">Ресторан</option>
-                                    <option value="4">Суши</option>
-                                </select>
-                            </div>
-
-                            <div class="photo input-field">
-                                <p>Фото:</p>
-                                <input type="text" placeholder="Выберите фото заведения"/>
-                                <div class="image-photo-placeholder">
-                                    <img class="photo-image" src="http://img.all-mods.ru/2016/04/skyrim-bolshe-markerov-dlya-npc-v-tavernax.jpg" alt="Фото заведения"/>
+                    <div class="col-2">
+                        <section id="map">
+                            <div id="map-placeholder">
+                                <p>Показано: 16 из 54 (очистить фильтры)</p>
+                                <div id="yandex-map-interactive">
+                                    <img src="https://s3-alpha.figma.com/img/dfc8/d74d/46ba1ca5be78af774d3bb2eafaf7dcfa?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJK6APQGEHTP6I3PA%2F20180608%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180608T235157Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=fc174611f04afe4b3a16d10255b6809449b3d9038b4b5bd784c88d830419aecb" alt="Yandex map"/>
                                 </div>
                             </div>
                         </section>
                     </div>
-                    <div class="col-2">
-                        <section id="map">
-                            <div id="map-placeholder">
-                                <p>Укажите место на карте:</p>
-                                <div id="yandex-map-interactive">
-                                    <img src="https://s3-alpha.figma.com/img/75ad/a671/e0aeb4e8dba89a90e75a40f83cd65abd?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJK6APQGEHTP6I3PA%2F20180527%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180527T233229Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=6f35e6f28d2bb8625b98565613c21637a2311f390b9660b5a48eeca24a05b6b1" alt="Yandex map"/>
+
+                    <div class="col-1">
+                        <section id="places-list">
+                            
+                            <div class="place-item">
+                            
+                                <div class="place-caption">
+                                    <div class="place-item-name">Holy Poli</div>
+                                    <div class="place-item-maintenance-elements">
+                                        <div class="place-item-edit-element"></div>
+                                        <div class="place-item-delete-element"></div>
+                                    </div>
+                                </div>
+                            
+                                <div class="place-item-rating">
+                                    <div class="place-item-rating-stars">
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                    </div>
+                                    <p class="place-item-rating-reviews-count">(Отзывов: 2)</p>
+                                </div>
+                            
+                                <div class="place-item-average-bill">
+                                    <p class="place-item-average-bill-caption">Средний чек: </p>
+                                    <p class="place-item-average-bill-value">600 р (35% стипендии)</p>
+                                </div>
+                            
+                                <div class="place-item-summary">
+                                    <p class="place-item-type">Бар</p>
+                                    <p class="place-item-address">Пожарный пер., 8</p>
                                 </div>
                             </div>
+
+                            <div class="place-item">
+                            
+                                <div class="place-caption">
+                                    <div class="place-item-name">Holy Poli</div>
+                                    <div class="place-item-maintenance-elements">
+                                        <div class="place-item-edit-element"></div>
+                                        <div class="place-item-delete-element"></div>
+                                    </div>
+                                </div>
+                            
+                                <div class="place-item-rating">
+                                    <div class="place-item-rating-stars">
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                    </div>
+                                    <p class="place-item-rating-reviews-count">(Отзывов: 2)</p>
+                                </div>
+                            
+                                <div class="place-item-average-bill">
+                                    <p class="place-item-average-bill-caption">Средний чек: </p>
+                                    <p class="place-item-average-bill-value">600 р (35% стипендии)</p>
+                                </div>
+                            
+                                <div class="place-item-summary">
+                                    <p class="place-item-type">Бар</p>
+                                    <p class="place-item-address">Пожарный пер., 8</p>
+                                </div>
+                            </div>
+
+                            <div class="place-item">
+                            
+                                <div class="place-caption">
+                                    <div class="place-item-name">Holy Poli</div>
+                                    <div class="place-item-maintenance-elements">
+                                        <div class="place-item-edit-element"></div>
+                                        <div class="place-item-delete-element"></div>
+                                    </div>
+                                </div>
+                            
+                                <div class="place-item-rating">
+                                    <div class="place-item-rating-stars">
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                    </div>
+                                    <p class="place-item-rating-reviews-count">(Отзывов: 2)</p>
+                                </div>
+                            
+                                <div class="place-item-average-bill">
+                                    <p class="place-item-average-bill-caption">Средний чек: </p>
+                                    <p class="place-item-average-bill-value">600 р (35% стипендии)</p>
+                                </div>
+                            
+                                <div class="place-item-summary">
+                                    <p class="place-item-type">Бар</p>
+                                    <p class="place-item-address">Пожарный пер., 8</p>
+                                </div>
+                            </div>
+
+                            <div class="place-item">
+                            
+                                <div class="place-caption">
+                                    <div class="place-item-name">Holy Poli</div>
+                                    <div class="place-item-maintenance-elements">
+                                        <div class="place-item-edit-element"></div>
+                                        <div class="place-item-delete-element"></div>
+                                    </div>
+                                </div>
+                            
+                                <div class="place-item-rating">
+                                    <div class="place-item-rating-stars">
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                        <div class="star"></div>
+                                    </div>
+                                    <p class="place-item-rating-reviews-count">(Отзывов: 2)</p>
+                                </div>
+                            
+                                <div class="place-item-average-bill">
+                                    <p class="place-item-average-bill-caption">Средний чек: </p>
+                                    <p class="place-item-average-bill-value">600 р (35% стипендии)</p>
+                                </div>
+                            
+                                <div class="place-item-summary">
+                                    <p class="place-item-type">Бар</p>
+                                    <p class="place-item-address">Пожарный пер., 8</p>
+                                </div>
+                            </div>
+
                         </section>
                     </div>
                 </div>
@@ -105,7 +224,7 @@
         <footer class="footer">
             <div class="left-space"></div>
             <div class="main-space">
-                SimbirSoft. 2018
+                SimbirSoft. 2018 <!--{{testMsg}}-->
             </div>
             <div class="right-space"></div>
         </footer>
@@ -114,11 +233,19 @@
 </template>
 
 <script>
+import './assets/styles.scss'
+
 export default {
-  name: 'App'
+  name: 'NewApp',
+  data: function () {
+    return {
+      testMsg: 'HELLO VAGIZ PROGRAMMER CODER COOL',
+      secondData: 100
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-
+    
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import NewApp from './App'
+import testComponent from './testComponent'
+// import router from './router'
 
-import './assets/styles.scss';
+// import './assets/styles.scss'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  // router,
+  components: { NewApp/*, testComponent*/ },
+  template: '<div id=\'app\'> <!--<testComponent title="OLOLO"/>--> <NewApp/></div>'
 })
